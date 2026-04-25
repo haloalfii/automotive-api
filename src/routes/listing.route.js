3dc8edc96473c4bb99d8d2866f451dcf0a33dbca
Alfian Luthfi <alfiankurniawan85@gmail.com>
2026-04-26 00:33:48 +0700

    Hotfix / Fix Route for Railway

--- src/routes/listing.route.js
+++ src/routes/listing.route.js
@@ -6,20 +6,27 @@ import {
   updateListing,
   deleteListing,
   searchListings,
-  suggestListings
+  suggestListings,
 } from "../controllers/listing.controller.js";
 
 import { validateCarId } from "../middlewares/validateCarId.js";
 
 const router = express.Router();
 
-router.get("/search", searchListings);
 router.get("/search/suggest", suggestListings);
+router.get("/search", searchListings);
 
 router.post("/", createListing);
 router.get("/", getListings);
+
 router.get("/:id", validateCarId, getListingById);
 router.patch("/:id", validateCarId, updateListing);
 router.delete("/:id", validateCarId, deleteListing);
+
+router.use((req, res) => {
+  res.status(404).json({
+    message: "Route not found",
+  });
+});
 
 export default router;
