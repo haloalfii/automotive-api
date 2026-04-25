e7ae6a1235589f108542ea4750bb8614166d98bb
Alfian Luthfi <alfiankurniawan85@gmail.com>
2026-04-25 21:33:51 +0700

    Initial Commit - Install Express

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.json({
+    message: "API is running 🚀",
+  });
+});
+
+export default app;
